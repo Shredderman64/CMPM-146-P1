abdd3d37948018c92b09e9d5c42fec82451aa93d
Ellie <151094173+EllieM-gd@users.noreply.github.com>
2024-06-28 20:27:51 -0700

    Code cleanup
    Removed most of the print statements, and useless lines

--- src/nm_pathfinder.py
+++ src/nm_pathfinder.py
@@ -19,16 +19,13 @@ def find_path (source_point, destination_point, mesh):
     path = []
     boxes = []
 
-    print(source_point)
-    print(destination_point)
-
     for box in mesh["boxes"]:
         if box[0] <= source_point[0] < box[1] and box[2] <= source_point[1] < box[3]:
             boxes.append(box)
-            source_box = box
+            source_box = box    #used to call simple_search
         if box[0] <= destination_point[0] < box[1] and box[2] <= destination_point[1] < box[3]:
             boxes.append(box)
-            dest_box = box
+            dest_box = box      
     
     if len(boxes) != 2:
         print("No path!")
@@ -37,7 +34,6 @@ def find_path (source_point, destination_point, mesh):
         path.append(destination_point)
     else:
         path, boxes = simple_search(source_box, dest_box, mesh,source_point,destination_point,boxes)
-    print(path)
     return path, boxes
 
 def simple_search(source_box, dest_box, mesh,source_point,destination_point,boxes):
@@ -70,35 +66,36 @@ def simple_search(source_box, dest_box, mesh,source_point,destination_point,boxe
         path.append(destination_point)      #set the start position of the line to the end position.
         while current_box != source_box:    #We end once we find last box.
             next_box = came_from[current_box]      #set a temporary copy of the box we are traveling to.
-            print(current_box)
-            print(source_box)
             #Save boxes as coordinates
             b1y1,b1y2,b1x1,b1x2 = current_box[0],current_box[1],current_box[2],current_box[3]   
             b2y1,b2y2,b2x1,b2x2 = next_box[0],next_box[1],next_box[2],next_box[3]
+
             #find range in the boxes
             xRange = [max(b1x1,b2x1),min(b1x2,b2x2)]    
             yRange = [max(b1y1,b2y1),min(b1y2,b2y2)]
-            print(xRange)
-            print(yRange)
+
             #find y location:
             if path[-1][0] < min(yRange):
                  tempY = min(yRange)
             elif path[-1][0] > max(yRange):
                 tempY = max(yRange)
-            else: tempY = path[-1][0]   #May need a better way to do this part
+            else: tempY = path[-1][0]   
+
             #find x location
             if path[-1][1] < min(xRange):
                 tempX = min(xRange)
             elif path[-1][1] > max(xRange):
                 tempX = max(xRange)
-            else: tempX = path[-1][1]   #same as above
+            else: tempX = path[-1][1]  
 
 
             #put it in path
             tempPath = tempY,tempX  #save the path with the values
-            print(tempPath)
+            #print(tempPath)
+            
             #boxes.append(current_box)  Uncomment this line for a cleaner amount of boxes ;)
             path.append(tempPath)       #push the path we just saved
-            current_box = came_from[current_box]    #traverse to next box (broken i think)
-        path.append(source_point)
+            current_box = came_from[current_box]    #traverse to next box
+        
+        path.append(source_point) #set last point in path to the source_point
         return path, boxes
